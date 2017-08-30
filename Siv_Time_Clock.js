fb6e78b7791768b714eeef7c219ac9601145d1fc
Sivli Kestanous <cmtoups@me.com>
2017-08-30 18:23:58 -0400

    time_clock init

---
+++ Siv_Time_Clock.js
@@ -0,0 +1,29 @@
+/**:
+  * @plugindesc (v1.0.0) [requires Siv_Plugin_Sanity v0.1.0] In-game time system UI
+  *
+  * Released under MIT license, https://github.com/Sivli-Embir/rpg_maker_plugins/blob/master/LICENSE
+  *
+  * @author Sivli Embir
+  *
+  * @help
+  * ============================================================================
+  * Documentation
+  * ============================================================================
+  *
+  * ============================================================================
+  * Change Log
+  * ============================================================================
+  *
+  * Version 1.0.0:
+  * - Finished plugin!
+  */
+
+///////////////////////////////////////////////////////////////////
+// Plugin registation with Siv_Plugin_Sanity dependency manager. //
+///////////////////////////////////////////////////////////////////
+ SIV_SCOPE.definePlugin({
+   name: 'Siv_Time_Clock',
+   plugin: function() {
+
+   }
+ })
